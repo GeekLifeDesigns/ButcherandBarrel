26b95285cb947439ce4a336e2bc28be39c2dbef0
GeekLifeDesigns <39569623+GeekLifeDesigns@users.noreply.github.com>
2018-08-06 08:58:07 -0500

    Add files via upload

---
+++ textnumber.php
@@ -0,0 +1,12 @@
+
+<?php
+require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
+use Twilio\Rest\Client;
+$client = new Client($sid, $token);
+$client->messages->create(
+    '+15558675309', // number to send to
+    array(
+        'from' => '+12623832300', // your Twilio number
+        'body' => "There’s something strange in my neighborhood. I don’t know who to call. Send help!"
+    )
+);
